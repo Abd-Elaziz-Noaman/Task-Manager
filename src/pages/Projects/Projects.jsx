14764779dc8f2e9d51cf739fb80127fab93fdf35
abdelaziz-noaman <zezo.noaman@yahoo.com>
2024-03-14 04:16:46 +0200

    implemented tabs in projects page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Typography,
   Breadcrumbs,
@@ -5,19 +6,29 @@ import {
   Tabs,
   Tab,
   Box,
-} from "@mui/material/Typography";
+  Badge,
+} from "@mui/material";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 
 import { MainContainer } from "./Projects.styled";
 
 export default function Projects() {
-  function handleClick(event) {
+  const [value, setValue] = useState("2");
+
+  const handleTabChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function handleBreadcrumbClick(event) {
     event.preventDefault();
     console.info("You clicked a breadcrumb.");
   }
   return (
     <MainContainer>
       <h3>Edit card</h3>
-      <div role="presentation" onClick={handleClick}>
+      <div role="presentation" onClick={handleBreadcrumbClick}>
         <Breadcrumbs aria-label="breadcrumb">
           <Link underline="hover" color="inherit" href="/">
             Dashboard
@@ -32,6 +43,41 @@ export default function Projects() {
           <Typography color="primary">photo shoot</Typography>
         </Breadcrumbs>
       </div>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box
+            sx={{
+              borderBottom: 1,
+              borderColor: "divider",
+            }}
+          >
+            <TabList
+              onChange={handleTabChange}
+              aria-label="lab API tabs example"
+            >
+              <Tab sx={{ textTransform: "none" }} label="General" value="1" />
+              <Tab
+                sx={{ textTransform: "none" }}
+                label={
+                  <>
+                    <p style={{ display: "inline-block" }}>Tasks</p>
+                    <Badge
+                      badgeContent={4}
+                      color="primary"
+                      sx={{ display: "inline-block" }}
+                    />
+                  </>
+                }
+                value="2"
+              />
+              <Tab label="Item Three" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
+      </Box>
     </MainContainer>
   );
 }
